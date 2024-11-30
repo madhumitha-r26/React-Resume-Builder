20f5fd810e158e4f19e5a1ae9752180938f91857
madhumitha-ravichandran26 <madhumitha26301@gmail.com>
2024-11-30 15:29:58 +0530

    completed the design

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -16,21 +16,21 @@ const Form = () => {
       <input
           type="text"
           class="w-full md:w-11/12 py-3 px-2 mt-3 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
-          placeholder="John Doe"
+          placeholder="Your Name"
         />
 
         <label className="mt-3">Job Title</label>
         <input
           type="text"
           class="w-full md:w-11/12 py-3 px-2 mt-3 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
-          placeholder="Software Engineer"
+          placeholder="Your Job Title"
         />
 
         <label className="mt-3">Email Id</label>
         <input
           type="email"
           class="w-full md:w-11/12 py-3 px-2 mt-3 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
-          placeholder="johndoe@gmail.com"
+          placeholder="Your Email"
         />
         
         <label className="mt-3">Phone Number</label>
@@ -54,28 +54,28 @@ const Form = () => {
       <input
           type="url"
           class="w-full md:w-11/12 py-3 px-2 mt-3 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
-          placeholder="https://www.linkedin.com/in/johndoe/"
+          placeholder="https://www.linkedin.com/in/"
         />
 
         <label className="mt-3">GitHub</label>
         <input
           type="url"
           class="w-full md:w-11/12 py-3 px-2 mt-3 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
-          placeholder="https://github.com/johndoe"
+          placeholder="https://github.com/"
         />
 
         <label className="mt-3">Website</label>
         <input
           type="url"
           class="w-full md:w-11/12 py-3 px-2 mt-3 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
-          placeholder="www.johndoe.com"
+          placeholder="https://www.yourwebsite.com/"
         />
         
         <label className="mt-3">Behance</label>
         <input
           type="url"
           class="w-full md:w-11/12 py-3 px-2 mt-3 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
-          placeholder="https://www.behance.net/johndoe"
+          placeholder="https://www.behance.net/"
         />
       </div>
       </form>
@@ -99,7 +99,7 @@ const Form = () => {
       <input
           type="text"
           class="w-full md:w-10/12 py-3 px-2 mt-3 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
-          placeholder="ABC University"
+          placeholder="Enter University"
         />
       </div>
       <div class="grid grid-cols-1 md:grid-cols-4 pt-3 ">
@@ -107,7 +107,7 @@ const Form = () => {
       <input
           type="text"
           class="w-full md:w-11/12 py-3 px-2 mt-3 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
-          placeholder="Bachelor of Technology"
+          placeholder="Enter Degree"
         />
 
         <label className="mt-3">Grade</label>
@@ -156,7 +156,7 @@ const Form = () => {
       <input
           type="text"
           class="w-full md:w-10/12 py-3 px-2 mt-3 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
-          placeholder="Amazon"
+          placeholder="Enter Company Name"
         />
       </div>
       <div class="flex flex-col md:flex-row pt-3">
@@ -164,7 +164,7 @@ const Form = () => {
       <input
           type="text"
           class="w-full md:w-10/12 py-3 px-2 mt-3 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
-          placeholder="Software Engineer"
+          placeholder="Enter Job Role"
         />
       </div>
       <div class="flex flex-col md:flex-row pt-3">
@@ -172,8 +172,8 @@ const Form = () => {
       <textarea
           type="text"
           class="w-full h-1/2 md:w-10/12 py-3 px-2 mt-3 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
-          placeholder="ABC University">
-        </textarea>
+          placeholder="Enter Job Description">       
+           </textarea>
       </div>
       <div class="grid grid-cols-1 md:grid-cols-4 pt-3 ">
 
@@ -198,7 +198,7 @@ const Form = () => {
    <fieldset>
    
    <div className="flex justify-between">
-   <legend className="pt-3 text-left font-medium text-2xl underline">Volunteering</legend>
+   <legend className="pt-3 text-left font-medium text-2xl underline">Projects</legend>
    <button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800">
  <AddIcon></AddIcon> ADD
 </button>
@@ -206,19 +206,11 @@ const Form = () => {
 
       <form>
       <div class="flex flex-col md:flex-row pt-3">
-      <label className="mt-3 mr-20">Organization </label>
+      <label className="mt-3 mr-20">Project Title</label>
       <input
           type="text"
           class="w-full md:w-10/12 py-3 px-2 mt-3 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
-          placeholder="Amazon"
-        />
-      </div>
-      <div class="flex flex-col md:flex-row pt-3">
-      <label className="mt-3 mr-28">Job Role</label>
-      <input
-          type="text"
-          class="w-full md:w-10/12 py-3 px-2 mt-3 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
-          placeholder="Software Engineer"
+          placeholder="Enter Project Title"
         />
       </div>
       <div class="flex flex-col md:flex-row pt-3">
@@ -226,7 +218,7 @@ const Form = () => {
       <textarea
           type="text"
           class="w-full h-1/2 md:w-10/12 py-3 px-2 mt-3 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
-          placeholder="ABC University">
+          placeholder="Enter Project Description">
         </textarea>
       </div>
       <div class="grid grid-cols-1 md:grid-cols-4 pt-3 ">
@@ -260,45 +252,111 @@ const Form = () => {
 
       <form>
       <div class="flex flex-col md:flex-row pt-3">
-      <label className="mt-3 mr-32">Name</label>
+      <label className="mt-3 mr-20 w-auto">Certificate Name</label>
       <input
           type="text"
-          class="w-full md:w-10/12 py-3 px-2 mt-3 mr-4 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
-          placeholder="Certification Name"
+          class="w-full md:w-full py-3 px-2 mt-3 mr-4 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
+          placeholder="Enter Certificate Name"
 
         />
       </div>
+
       <div class="flex flex-col md:flex-row pt-3">
-      <label className="mt-3 mr-32">Issuer</label>
+      <label className="mt-3 mr-20 w-auto">Certificate Link</label>
+      <input
+          type="url"
+          class="w-full md:w-full py-3 px-2 mt-3 mr-4 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
+          placeholder="Enter Certificate Link"
+
+        />
+      </div>
+
+      
+      
+      <div class="grid grid-cols-1 md:grid-cols-4 pt-3">
+      <label className="mt-3">Issuer</label>
       <select 
         class="w-full md:w-10/12 py-3 px-2 mt-3 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none">
          <option>- Select -</option>
          <option>Coursera</option>
          <option>Microsoft</option>
          <option>GUVI</option>
+         <option>Great Learning</option>
          <option>Simplilearn</option>
          <option>Udemy</option>
          <option>Google</option>
          <option>Cisco</option>
+         <option>Others</option>
         </select>
 
-      </div>
-      <div class="flex flex-col md:flex-row pt-3">
-      <label className="mt-3 mr-32">Link</label>
-      <input
-          type="text"
-          class="w-full md:w-10/12 py-3 px-2 mt-3 mr-4 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
-          placeholder="Certification URL"
+        <label className="mt-3">Issued Date</label>
+        <input
+          type="month"
+          class="w-full md:w-11/12 py-3 px-2 mt-3 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
         />
       </div>
+
 
       </form>
     </fieldset>
     <hr/>
+   {/* ---------------------------------------------------------------------------------------- */}
 
+   <fieldset>
+   
+   <div className="flex justify-between">
+   <legend className="pt-3 text-left font-medium text-2xl underline">Volunteering</legend>
+   <button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800">
+ <AddIcon></AddIcon> ADD
+</button>
+   </div>
+
+      <form>
+      <div class="flex flex-col md:flex-row pt-3">
+      <label className="mt-3 mr-20">Organization </label>
+      <input
+          type="text"
+          class="w-full md:w-10/12 py-3 px-2 mt-3 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
+          placeholder="Enter Organization Name"
+        />
+      </div>
+      <div class="flex flex-col md:flex-row pt-3">
+      <label className="mt-3 mr-28">Job Role</label>
+      <input
+          type="text"
+          class="w-full md:w-10/12 py-3 px-2 mt-3 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
+          placeholder="Enter Volunteer Role"
+        />
+      </div>
+      <div class="flex flex-col md:flex-row pt-3">
+      <label className="mt-3 mr-20">Description</label>
+      <textarea
+          type="text"
+          class="w-full h-1/2 md:w-10/12 py-3 px-2 mt-3 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
+          placeholder="Enter Volunteering Description">
+        </textarea>
+      </div>
+      <div class="grid grid-cols-1 md:grid-cols-4 pt-3 ">
+
+        <label className="mt-3">Start-Date</label>
+        <input
+          type="month"
+          class="w-full md:w-11/12 py-3 px-2 mt-3 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
+        />
+        
+        <label className="mt-3">End-Date</label>
+        <input
+          type="month"
+          class="w-full md:w-11/12 py-3 px-2 mt-3 xl:block border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none"
+        />
+      </div>
+      </form>
+    </fieldset>
+    <hr/>
 
    {/* ---------------------------------------------------------------------------------------- */}
 
+  
    <fieldset>
    
    <div className="flex justify-between">
